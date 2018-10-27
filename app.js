c9c410bd8623d5ee029ed18220ae68c7bc22f1a7
Amy Hart <xamyhart@gmail.com>
2018-10-27 17:58:24 +0100

    Basic Express API routes created

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { createQuestion, getQuestions, getQuestionById } = require('./controllers/questions'); 
+
+const dotenv = require('dotenv').config({
+  path: './settings.env',
+});
+
+const app = express();
+app.use(bodyParser.json());
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// POST a question
+app.post('/questions/', createQuestion);
+
+// GET all questions
+app.get('/questions', getQuestions);
+
+// GET a specific question by ID
+app.get('/questions/:questionID', getQuestionById);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
