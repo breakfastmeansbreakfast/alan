5f729158ef90cbdc2512fa44548ed50377d8d782
breakfastmeansbreakfast <77744c@gmail.com>
2018-10-28 09:06:24 +0000

    hardcoded responses added

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const sendsms = require('./src/sendsms');
 
-const phoneno = '447787168962';
-const messagecontent = 'SAY THAT AGAIN';
-
 const app = require('./app');
-
-// sendsms.sendresponse(phoneno, messagecontent);
